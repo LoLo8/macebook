c9896d76d39f623c230a83eb80aaf381872642d5
Joshua Peters <jpeters280@gmail.com>
2016-09-27 11:14:49 -0700

    removed login

--- mean_login_reg/server/config/routes.js
+++
@@ -1,7 +0,0 @@
-console.log('routes');
-var users = require('../controllers/users.js');
-// What is this 'friends' object we are referencing below??
-module.exports = function(app){
-  app.post('/register', users.create);
-  app.post('/login', users.login);
-}
